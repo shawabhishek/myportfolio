a10b438a548520897a4a5883612217c74bddee86
abhishek <shaw.rockshek66@gmail.com>
2020-08-30 20:33:23 +0530

    Updates

---
+++ precache-manifest.445f71c4570ebc0e1b806bf6667fe995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29770a9489347378a41dea0bf1131ef",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "ebb153b74c33ce4ae8ce",
+    "url": "/myportfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "c1f02aa3f7c0d3c5cb3d",
+    "url": "/myportfolio/static/css/main.0c52a056.chunk.css"
+  },
+  {
+    "revision": "ebb153b74c33ce4ae8ce",
+    "url": "/myportfolio/static/js/2.66c7c852.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myportfolio/static/js/2.66c7c852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f02aa3f7c0d3c5cb3d",
+    "url": "/myportfolio/static/js/main.eb826aff.chunk.js"
+  },
+  {
+    "revision": "27ff665cd96372b8b66a",
+    "url": "/myportfolio/static/js/runtime-main.047ac6ea.js"
+  }
+]);
